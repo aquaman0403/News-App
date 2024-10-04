4baef3305f04131476206c6a0e1cfad3589272d0
Vladimir Duck <vanducpham2k3nh@gmail.com>
2024-10-04 18:56:24 +0700

    first change

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val USER_SETTINGS = "user_settings"
+    const val APP_ENTRY = "app_entry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "b6165f3719624abc9cf2c04d7af64300"
+
+}
